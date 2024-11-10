bf60abe9fda025ba1532d5e8163f7128af1157ff
SelamB <selambeyene2@gmail.com>
2024-11-09 16:24:59 -0800

    sets up product model

---
+++ src/Model/Product.java
@@ -0,0 +1,28 @@
+package Model;
+
+public class Product {
+    long productId;
+    String productName;
+    String productDescription;
+
+    public long getProductId() {
+        return productId;
+    }
+    public String getProductName() {
+        return productName;
+    }
+    public String getProductDescription() {
+        return productDescription;
+    }
+    public long setProductId(long productId) {
+        return this.productId = productId;
+    }
+    public String setProductName(String productName) {
+        return this.productName = productName;
+    }
+    public String setProductDescription(String productDescription) {
+        return this.productDescription = productDescription;
+    }
+
+    
+}
